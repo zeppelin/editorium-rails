fead14b412975f99af98c860e050178fb8d77810
Gabor Babicz <gabor.babicz@gmail.com>
2015-10-08 14:40:31 +0200

    Default to HTTP

--- lib/editorium/config.rb
+++ lib/editorium/config.rb
@@ -12,7 +12,7 @@ module Editorium
     attr_accessor :service_url, :preview_layout
 
     def initialize
-      @service_url = 'https://editorium.herokuapp.com'
+      @service_url = 'http://editorium.herokuapp.com'
       @preview_layout = false
     end
   end
